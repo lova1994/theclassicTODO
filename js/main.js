9e27cbab5b44eef716004ca44548f7e99926a2b3
Lova Wins <lovaunger@gmail.com>
2019-06-16 19:05:52 +0200

    init commit
    the basic simple todo list

---
+++ js/main.js
@@ -0,0 +1,147 @@
+"Use strict";
+
+// variabler
+
+var newToDoEl = document.getElementById("newtodo");
+var newToDoButtonEl = document.getElementById("newtodobutton");
+var messageEl = document.getElementById("message");
+var toDoListEl = document.getElementById("todolist");
+var i;
+var clearButtonEl = document.getElementById("clearbutton");
+var todo = document.getElementsByClassName("todo");
+
+// Händelsehanterare;
+newToDoEl.addEventListener("keyup", checkInput, false);
+newToDoButtonEl.addEventListener("click", addItem, false);
+window.onload = init;
+clearButtonEl.addEventListener("click", clearStorage, false)
+
+
+// Startfunktion
+function init() {
+    console.log("Initierar...");
+
+    // Inaktivera lägg till-knappen
+    newToDoButtonEl.disabled = true
+
+    // läs in kurslista
+    if (todo != null) {
+        loadStorage();
+    }
+}
+
+// Kontrollera input
+function checkInput() {
+    console.log("Kontrollerar input");
+
+    var input = newToDoEl.value;
+
+    // Kontroll om korrekt längd
+
+    if (input.length > 4) {
+        messageEl.innerHTML = "";
+        newToDoButtonEl.disabled = false
+
+    } else {
+        messageEl.innerHTML = "Måste vara minst fem tecken!"
+        newToDoButtonEl.disabled = true
+    }
+}
+
+
+// Lägga till kurs
+function addItem() {
+    console.log("Lägger till kurs...");
+
+
+    // Skapar nytt element
+    var input = newToDoEl.value;
+
+    var newEl = document.createElement("article");
+    var newTextNode = document.createTextNode(input);
+
+    newEl.appendChild(newTextNode);
+    newEl.className = "todo";
+
+    // Lägger till i listan
+    toDoListEl.appendChild(newEl);
+
+    // Lägg till en klickhanterare
+    newEl.addEventListener("click", function (e) {
+        e.target.remove();
+        storeItem();
+    });
+    // Raderar input-fält
+    newToDoEl.value = "";
+    newToDoButtonEl.disabled = true
+
+    // anropar lagring
+    storeItem();
+}
+
+
+// Spara kurser 
+function storeItem() {
+    console.log("Lagrar lista...");
+
+    // Läs in i kurslistan
+    var todo = document.getElementsByClassName("todo");
+    var tempArr = [];
+
+    // Loopa genom listan och lagrar till temporär array
+    for (i = 0; i < todo.length; i++) {
+        tempArr.push(todo[i].innerHTML);
+    }
+
+    // Konverterar till JSON-sträng
+    var jsonStr = JSON.stringify(tempArr);
+
+    // Lagra i web storage
+    localStorage.setItem("todo", jsonStr);
+    console.log(tempArr);
+}
+
+
+// Läs in kurser
+
+function loadStorage() {
+    console.log("Läser in lista...")
+
+    // Läs in och konvertera från JSON till array
+    var todo = JSON.parse(localStorage.getItem("todo"));
+    
+    // Loopa genom arrayen
+    for (i = 0; i < todo.length; i++) {
+
+        // Skapa nya element
+        var newEl = document.createElement("article");
+        var newTextNode = document.createTextNode(todo[i]);
+        newEl.appendChild(newTextNode);
+        newEl.className = "todo";
+
+        // Lägger till i listan
+        toDoListEl.appendChild(newEl);
+
+
+
+        // Lägg till en klickhanterare
+        newEl.addEventListener("click", function (e) {
+            e.target.remove()
+            console.log("Raderar")
+
+            // Lagra listan på nytt
+            storeItem();
+
+        });
+    }
+}
+
+// Clear storage och lista-funktion
+function clearStorage() {
+    console.log("Raderar")
+    localStorage.clear();
+    toDoListEl.innerHTML = ""
+
+}
+
+
